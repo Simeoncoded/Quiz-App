3ac002f25f7a52d5646f67fae1045b91e36c20cc
Simeoncoded <simeonolawore07@gmail.com>
2024-05-09 15:37:46 -0400

    Second commmit

---
+++ src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Quiz.css";
+
+
+export default function Quiz(){
+    return <div>
+        <h1>Quiz App</h1>
+        <hr />
+        <h3>Which device is required for the Internet connection?</h3>
+        <ul>
+            <li>Modem</li>
+            <li>Router</li>
+            <li>Lan Cable</li>
+            <li>Pen Drive</li>
+        </ul>
+    </div>
+}
