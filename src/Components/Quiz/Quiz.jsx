a7a8f99defead0a00e54f52797529cb32e4c35d0
Simeoncoded <simeonolawore07@gmail.com>
2024-05-09 16:25:49 -0400

    Eighth Commit

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -20,6 +20,10 @@ export default function Quiz(){
 
     const checkAns = (e,ans) => {
         if(lock === false){
+            if(index === data.length-1){
+                setResult(true);
+                return 0;
+            }
         if(question.ans===ans){
             e.target.classList.add("correct");
             setLock(true);
@@ -50,7 +54,7 @@ export default function Quiz(){
     return <div className="container">
         <h1>Quiz App</h1>
         <hr />
-        <h2>{index+1}. {question.question}</h2>
+        {result ?<></>:<><h2>{index+1}. {question.question}</h2>
         <ul>
             <li ref={Option1} onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
             <li ref={Option2} onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
@@ -58,6 +62,7 @@ export default function Quiz(){
             <li ref={Option4} onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
         </ul>
         <button onClick={next}>Next</button>
-        <div className="index">{index +1} of {data.length} Questions</div>
+        <div className="index">{index +1} of {data.length} Questions</div></>}
+        
     </div>
 }
