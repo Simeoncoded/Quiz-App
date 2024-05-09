48a9362446d80af7ab28db311b25a41a2617bbc1
Simeoncoded <simeonolawore07@gmail.com>
2024-05-09 16:04:58 -0400

    fourth commit

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Quiz.css";
-
+import { data } from "../../assets/data";
 
 export default function Quiz(){
+
+    let [index,setIndex] = useState(0);
+    let [question,setQuestion] = useState(data[index]);
+    let [lock,setLock] = useState(false);
+
+    let Option1 = useRef(null);
+    let Option2 = useRef(null);
+    let Option3 = useRef(null);
+    let Option4 = useRef(null);
+
+    let optionArray = [Option1,Option2,Option3,Option4];
+
+    const checkAns = (e,ans) => {
+        if(lock === false){
+        if(question.ans===ans){
+            e.target.classList.add("correct");
+            setLock(true);
+        }
+        else{
+            e.target.classList.add("wrong");
+            setLock(true);
+        }
+    }
+    }
     return <div className="container">
         <h1>Quiz App</h1>
         <hr />
-        <h3>Which device is required for the Internet connection?</h3>
+        <h2>{index+1}. {question.question}</h2>
         <ul>
-            <li>Modem</li>
-            <li>Router</li>
-            <li>Lan Cable</li>
-            <li>Pen Drive</li>
+            <li onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+            <li onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
+            <li onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+            <li onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
         </ul>
         <button>Next</button>
         <div className="index">1 of 5 Questions</div>
